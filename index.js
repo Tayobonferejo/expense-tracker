1264d46f4b3c23612ada7451af5408b30f47eea8
tayobonferejo <oluwatayogeorgeige@gmail.com>
2026-03-03 23:46:50 +0100

    started working with function of choice

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
+let choice = "income"
+
+let parentOfChoice = document.querySelector(".type-toggle");
+const expenseChoice = document.getElementById("expense");
+const incomeChoice = document.getElementById("income");
+
+parentOfChoice.addEventListener("click", function(event){
+    console.log(event.target);
+
+    if(event.target.id === "expense") {
+        expenseChoice.classList.add("choice");
+        incomeChoice.classList.remove("choice");    
+        choice = "expense"  
+    }
+
+    if(event.target.id === "income") {
+        expenseChoice.classList.remove("choice");
+        incomeChoice.classList.add("choice");     
+        choice = "income"
+    }
+
+})
 
 
